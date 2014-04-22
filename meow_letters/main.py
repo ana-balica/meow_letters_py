7e901b49c1f168dd01db675ff8f626391e6454a5
ana-balica <ana.balica@gmail.com>
2014-04-22 18:26:44 +0200

    create basic screen manager for kivy app

--- meow_letters/main.py
+++ meow_letters/main.py
@@ -1,19 +1,33 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
-from kivy.vector import Vector
-from kivy.clock import Clock
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 
 
+class MenuScreen(Screen):
+    pass
 
-class MeowLettersGame(Widget):
-	pass
+class GameScreen(Screen):
+    pass
+
+class GameOverScreen(Screen):
+    pass
+
+class HighscoresScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+    pass
 
 
 class MeowLettersApp(App):
-	def build(self):
-		return MeowLettersGame()
+    def build(self):
+        root = ScreenManager(transition=NoTransition())
+        root.add_widget(MenuScreen(name='menu'))
+        root.add_widget(GameScreen(name='game'))
+        root.add_widget(GameOverScreen(name='gameover'))
+        root.add_widget(HighscoresScreen(name='highscores'))
+        root.add_widget(SettingsScreen(name='settings'))
+        return root
 
 
 if __name__ == '__main__':
-	MeowLettersApp().run()
+    MeowLettersApp().run()
