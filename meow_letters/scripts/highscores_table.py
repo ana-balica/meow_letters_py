babc8794819cd2b7926efdf1b50fab9a190e527c
ana-balica <ana.balica@gmail.com>
2014-05-10 23:45:12 +0200

    fix create highscores table script

--- meow_letters/scripts/highscores_table.py
+++ meow_letters/scripts/highscores_table.py
@@ -3,5 +3,5 @@ from meow_letters.storage.meowdb import MeowDatabase
 
 database = MeowDatabase()
 database.db.execute("""CREATE TABLE highscores (id integer primary key autoincrement,
-                       username text, highscore integer)""", commit=True)
+                       username text, highscore integer)""")
 database.db.close()
